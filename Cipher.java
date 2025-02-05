4dd6aa8a9d5a8a8f7a2a77959ca3ebaa5fc03762
fatima493 <fatima.hajiyeva@ug.bilkent.edu.tr>
2025-02-05 11:20:57 +0300

    Updating Cipher.java
    filled in empty loops, updated the outputs, replaced unnecessary loops with if/else, and checked the empty result case.

--- Cipher.java
+++ Cipher.java
@@ -1,56 +1,54 @@
 public class Cipher {
-        // Strings for keeping the alphabets, one for the original letters and the other for the encrypted ones
         public static final String ORIGINAL_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
         public static final String CIPHER_ALPHABET = "dfxyhrklvwuasgimnojpqetbcz";
     
-        // Method to encrypt a string
+        // Encrypting the string
         public String encrypt(String inputString) {
             String outputString = "";
     
-            // Fill in the for loop
+            // Loop was empty
             for (int i = 0; i < inputString.length(); i++) {
                 char currentChar = inputString.charAt(i);
-                // Replace the character
+                // replaceChar
                 outputString += replaceChar(currentChar, true);
             }
     
             return outputString;
         }
     
-        // Decrypt method
+        // Decrypting the string
         public String decrypt(String inputString) {
             String outputString = "";
     
-            // Fill in the for loop
+            // Loop was empty
             for (int i = 0; i < inputString.length(); i++) {
                 char currentChar = inputString.charAt(i);
-                // Make sure the outputString is updates
+                // Updating the output
                 outputString += replaceChar(currentChar, false);
             }
     
             return outputString;
         }
     
-        // Replace the input
+        // replaceChar
         private char replaceChar(char inputChar, boolean isEncrypt) {
-            // Convert to lowercase to handle case-insensitive matching
+            // check case-senstivity
             char lowerChar = Character.toLowerCase(inputChar);
     
-            //Replace the for loops with if/else statements
-            // Check if the character is in the alphabet
+            //Replaced for loops with if/else + check if the character is in the alphabet
             if (ORIGINAL_ALPHABET.indexOf(lowerChar) != -1) {
                 if (isEncrypt) {
-                    // Revert alphabet to the cipher alphabet
+                    // Alphabet to the cipher
                     int index = ORIGINAL_ALPHABET.indexOf(lowerChar);
                     return CIPHER_ALPHABET.charAt(index);
                 } else {
-                    // Revert cipher to the alphabet
+                    // Cipher to the alphabet
                     int index = CIPHER_ALPHABET.indexOf(lowerChar);
                     return ORIGINAL_ALPHABET.charAt(index);
                 }
             }
     
-            // If the character is not in the alphabet (e.g., space, punctuation), return it as is
+            // If alphabet does not contain the char
             return inputChar;
         }
     }
